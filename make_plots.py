990aea808be18a29223c34ea727e14f77e96f7f9
Dawid Gerstel <dawidgerstel@gmail.com>
2017-05-07 17:20:13 +0200

    add add-hist in class

--- make_plots.py
+++ make_plots.py
@@ -6,12 +6,12 @@ from ROOT import kBlue, kRed, kOrange, kYellow, kGreen, kMagenta,\
 from os import listdir
 from os.path import join
 import ROOT
-# from ROOT import Rtypes
 import collections
 
 import numpy as np
 
 # Get all ntuples to be plotted
+#path = "/media/sdb1/HEP_DATA/IsoVars/"
 path = "/lhcb5/users/gerstel/analysis/"
 root_file = "DVNtuple.root"
 # all decay modes
@@ -37,6 +37,7 @@ modes = collections.OrderedDict(modes)
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 mode_list = [m for m in flatten(modes.values())]
+#mode_list = [mode_list[3], mode_list[15]]
 
 fnames = [join(path, direc, root_file) for direc in mode_list]
 print "MODES: ", mode_list, "\n\n"
@@ -104,7 +105,7 @@ my_leaves = [l for l in iso_leaves if "Dawid" in l]
 not_my_leaves = [l for l in iso_leaves if not "Dawid" in l]
 
 # Group my_leaves according to input var set
-CONFIG = [str(i) for i in [1,3,4,5]]
+CONFIG = [str(i) for i in [1,3]]
 same_var_l = [[l for l in my_leaves if "var"+config in l] for config in CONFIG ]
 print "** SAME VAR LEAVES: ", same_var_l
 # min_{all-tracks}(BDT) for each input var set
@@ -117,14 +118,13 @@ min_bdtg_per_evt =["min(min(" + ",".join(svl[:2]) + "),min(" + \
 ######################################################################
 leaves_subset = important_old_leaves[:2] #not_my_leaves[:2] #min_bdtg_per_evt
 
+#OUTDIR = "./plots/"
 OUTDIR = "/lhcb/users/gerstel/LHCb_Analysis/DaVinciDev_v37r2p4/plots/"
 OUTFILE = OUTDIR + "old_iso_vars.pdf"
 
-MERGE_OUTPUT = True
 ######################################################################
 # Allocate histograms
 ######################################################################
-h = [[] for m in mode_list]
 
 #h_ bdtg = {}
 # for j,l in enumerate(leaves_subset):
@@ -144,83 +144,9 @@ h = [[] for m in mode_list]
 # Superimpose hist #
 ######################################################################
 
-cnvs = [TCanvas("c"+str(i)) for i in range(len(leaves_subset))]
-#cnvs = TCanvas("c")
-#cnvs.Divide(3,2)
-for c in cnvs:
-#  c.Divide(3, 2)
-  c.Divide(6, 3)
-
 def get_hist_label(j, i):
   """ Helper to uniquely name histograms """
   return "bdtg" + "j" + str(j) + "i" + str(i)
-
-
-def plot_my_leave(leaf_id, c, ymax_all=None, xmax_all=None):
-  """
-  params:
-  =======
-  align - Boolean (Optional) - make x-axis ranges same for all plots on 
-          the canvas
-  """
-  global h
-  bdtg_label = []
-  legs = [TLegend(0.2,0.5,0.6,0.9) for group in mode_list]
-
-  # Global (full canvas) xmin and xmax for alignment
-  xmin_g, xmax_g = (0, 0)
-  mode_id = -1
-  for group_id, (group, decays) in enumerate(modes.iteritems()):
-    for mode in decays:
-      mode_id += 1
-      print "\nSuperimpose modes: ", mode
-      c.cd(mode_id + 1) #group_id+1)
-      bdtg_label.append(get_hist_label(leaf_id, mode_id))
-      print "*** BDTG_LABEL: ", bdtg_label[-1]
-      draw_cmd = leaves_subset[leaf_id]+">>"+bdtg_label[-1]
-      trees[mode_id].Draw(draw_cmd, "", "goff")
-      htmp = gDirectory.Get(bdtg_label[-1])
-
-      # Update minima/maxima
-      xmin, xmax = (htmp.GetXaxis().GetXmin(), htmp.GetXaxis().GetXmax())
-      if xmin < xmin_g: xmin_g = xmin
-      if xmax > xmax_g: xmax_g = xmax
-
-
-  print "xmax_all = ", xmax_all
-  print "xmax_g = ", xmax_g
-
-  # Draw the leaves into the histograms
-  # Their same binning and ranges help in making the ROC curve
-  for mode_id, mode in enumerate(mode_list):
-
-    # Define histogram
-    label = mode+leaves_subset[leaf_id]
-    if xmax_all is not None:
-      h[mode_id].append(TH1F(label, leaves_subset[leaf_id], 100, xmin_g, xmax_all))
-    else:
-      h[mode_id].append(TH1F(label, leaves_subset[leaf_id], 100, xmin_g, xmax_g))
-    if ymax_all is not None:
-      h[mode_id].SetMaximum(ymax_all)
-
-    # Draw histogram with legend
-    c.cd(mode_id + 1)
-    draw_cmd = leaves_subset[leaf_id]+">>"+label
-    trees[mode_id].Draw(draw_cmd, "")
-    # Normalise hist
-    norm =  h[mode_id][leaf_id].Integral("width")
-    print "NORM = ", norm
-    if norm:  # normalise non-empty hists
-      h[mode_id][leaf_id].Scale(1. / norm)
-    else:
-      print "Mode: ", mode_list[mode_id], " has empty hist"
-    legs[mode_id].AddEntry(mode, mode_list[mode_id], 'l')
-    legs[mode_id].Draw()
-
-
-  # Update and save canvas to single pdf page
-  c.Update()  # Important also for axes alignment
-  c.Print("plots/output.pdf")
 
 
 class ROC(object):
@@ -263,23 +189,121 @@ class IsolationAnalyser(object):
   """
   
   def __init__(self):
-    self.h = None
+    self.hists = [[] for m in mode_list]
+
+  def __call__(self):
+    self.make_raw_hists()
+    self.merge_signals()
+    self.merge_backgrounds()
+
+  def make_raw_hists(self):
+    xmax_l = [30000, 1.5]
+    # Open canvas and output file
+    C = TCanvas()
+    C.Print("plots/output.pdf[")
+
+    for i,l in enumerate(leaves_subset):
+      self.plot_my_leave(leaf_id=i, ymax_all=None, xmax_all=xmax_l[i])
+
+    # Close the output file
+    C.Print("plots/output.pdf]")
+
+  def add_hists(self, h_list, label, title):
+    his0 = h_list[0]
+    nbins = his0.GetNbinsX()
+    xmax = his0.GetXaxis().GetXmax()
+    xmin = his0.GetXaxis().GetXmin()
+    res = TH1F(label, title, nbins, xmin, xmax)
+    for i in range(h_list[0].GetNbinsX()+1):
+      bin_centre = h_list[0].GetBinCenter(i)
+      res.Fill(bin_centre, sum(h.GetBinContent(i) for h in h_list))
+    return res
+      
+
+  def merge_signals(self):
+    #self.sig_merged = [self.add_hists(
+    pass
+    #self.sig_h = [self.hists[0][i] for i,l in enumerate(leaves_subset) if l in ['Bs_mutau,pipipinu' , 'Bd_mutau,pipipinu']] 
+    #self.sig_h = self.merge_hists(self.sig_h[0], self.sig_h[1], label="test", title="testnig")
+
+  def merge_backgrounds(self):
+    pass
+    #self.bkg_h = [self.hists[1][i] for i,l in enumerate(leaves_subset)if "Dstst" in l] 
+
+  def make_ROCs(self):
+    for sig,bkg in zip(self.sig_h, self.bkg_h):
+      roc = ROC(sig, bkg)
+      xx, yy = roc()
+      #plt.plot(xx, yy, label=
+
+  def plot_my_leave(self, leaf_id, ymax_all=None, xmax_all=None):
+    """
+    params:
+    =======
+    """
+    c = TCanvas()
+    c.Divide(6, 3)
+    bdtg_label = []
+    legs = [TLegend(0.2,0.5,0.6,0.9) for group in mode_list]
+
+    # Global (full canvas) xmin and xmax for alignment
+    xmin_g, xmax_g = (0, 0)
+    mode_id = -1
+    for mode in mode_list:
+      mode_id += 1
+      print "\nSuperimpose modes: ", mode
+      c.cd(mode_id + 1) #group_id+1)
+      bdtg_label.append(get_hist_label(leaf_id, mode_id))
+      print "*** BDTG_LABEL: ", bdtg_label[-1]
+      draw_cmd = leaves_subset[leaf_id]+">>"+bdtg_label[-1]
+      trees[mode_id].Draw(draw_cmd, "", "goff")
+      htmp = gDirectory.Get(bdtg_label[-1])
+
+      # Update minima/maxima
+      xmin, xmax = (htmp.GetXaxis().GetXmin(), htmp.GetXaxis().GetXmax())
+      if xmin < xmin_g: xmin_g = xmin
+      if xmax > xmax_g: xmax_g = xmax
+
+      print "xmax_all = ", xmax_all
+      print "xmax_g = ", xmax_g
+
+    # Draw the leaves into the histograms
+    # Their same binning and ranges help in making the ROC curve
+    for mode_id, mode in enumerate(mode_list):
+
+      # Define histogram
+      label = mode+leaves_subset[leaf_id]
+      if xmax_all is not None:
+        self.hists[mode_id].append(TH1F(label, leaves_subset[leaf_id], 100, xmin_g, xmax_all))
+      else:
+        self.hists[mode_id].append(TH1F(label, leaves_subset[leaf_id], 100, xmin_g, xmax_g))
+      if ymax_all is not None:
+        self.hists[mode_id].SetMaximum(ymax_all)
+
+      # Draw histogram with legend
+      c.cd(mode_id + 1)
+      draw_cmd = leaves_subset[leaf_id]+">>"+label
+      trees[mode_id].Draw(draw_cmd, "")
+      # Normalise hist
+      norm =    self.hists[mode_id][leaf_id].Integral("width")
+      print "NORM = ", norm
+      if norm:    # normalise non-empty hists
+        self.hists[mode_id][leaf_id].Scale(1. / norm)
+      else:
+        print "Mode: ", mode_list[mode_id], " has empty hist"
+        legs[mode_id].AddEntry(mode, mode_list[mode_id], 'l')
+        legs[mode_id].Draw()
+
+
+    # Update and save canvas to single pdf page
+    c.Update()  # Important also for axes alignment
+    c.Print("plots/output.pdf")
+
 ######################################################################
 # MAIN
 ######################################################################
-
-xmax_l = [30000, 1.5]
-# Open canvas and output file
-C = TCanvas()
-C.Print("plots/output.pdf[")
-
-
-for i,l in enumerate(leaves_subset):
-  plot_my_leave(leaf_id=i, c=cnvs[i], ymax_all=None, xmax_all=xmax_l[i])
-
-
-# Close the output file
-C.Print("plots/output.pdf]")
+isoAn = IsolationAnalyser()
+isoAn()
 
 
 
